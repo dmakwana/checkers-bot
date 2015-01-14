a4d53777f069c3c7becea5be2c369895020f3532
Digvijay Makwana <dmakwana@github.com>
2015-01-14 02:57:50 -0500

    removed unnecessary test code from the test script

--- test.py
+++ test.py
@@ -9,10 +9,6 @@ game.start_game()
 
 moves = game.possible_moves()
 
-# for nodeRow in game.listOfNodes:
-# 	for node in nodeRow:
-# 		print node.colour
-
 while len(moves) > 0:
 	states = game.get_states_for_list_of_moves(moves)
 	choice = random.choice(states)
@@ -20,9 +16,6 @@ while len(moves) > 0:
 		sys.exit(-1)
 	print choice
 	moves = game.possible_moves()
-	# for nodeRow in game.listOfNodes:
-	# 	for node in nodeRow:
-	# 		print node.colour
 	# sleep(0.1)
 
 game.end_game()
